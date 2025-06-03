08812ba8137c69c065e41bc9ce2cdcf2c5d6090e
시원 <sw622@naver.com>
2025-06-03 16:55:22 +0900

    ✨ Add JS Deep Dive 16-08 Example - Based On Chapter 16.4

--- ch16/16-04.js
+++ ch16/16-04.js
@@ -28,4 +28,29 @@ person.lastName = "Kim";
 console.log(person.lastName); // Lee
 
 delete person.lastName;
-console.log(person.lastName);
+console.log(person.lastName); // Lee
+
+descriptor = Object.getOwnPropertyDescriptor(person, "lastName");
+console.log("lastName", descriptor);
+// lastName { value: 'Lee', writable: false, enumerable: false, configurable: false }
+
+Object.defineProperty(person, "fullName", {
+  get() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set(name) {
+    [this.firstName, this.lastName] = name.split(" ");
+  },
+  enumerable: true,
+  configurable: true,
+});
+
+descriptor = Object.getOwnPropertyDescriptor(person, "fullName");
+console.log("fullName", descriptor);
+// fullName { get: [Function: get], set: [Function: set], enumerable: true, configurable: true }
+
+person.fullName = "Heegun Lee";
+console.log(person.fullName); // Heegun Lee
+console.log(person); // { firstName: 'Heegun', fullName: [Getter/Setter] }
+
+/* 16-09 */
